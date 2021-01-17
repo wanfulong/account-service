e5c27acfd28e5993f6af33ea865c2d8e234373ca
wanfulong <wfl15938038269@sina.com>
2021-01-17 14:28:42 +0800

    log

---
+++ src/com/gitservice/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.gitservice;
+
+/**
+ * @author wanfulong
+ * @date 2021/1/17 - 13:51
+ **/
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hollow World!");
+    }
+}
